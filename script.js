cfb7d2c4190a5125f232b611ebb8d4e8e5546afe
ribondareva <msbondareva01@gmail.com>
2025-04-24 13:51:04 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const content = btn.nextElementSibling;
+    content.style.display = content.style.display === "block" ? "none" : "block";
+  });
+});
